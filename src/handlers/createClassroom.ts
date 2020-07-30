c2f8c55980ed0210ae20143d0f289bd813ead5ff
BlueGhostGH <engisoftleaderoff@gmail.com>
2020-07-31 02:09:11 +0300

    feat: added a context type for handling data

--- src/handlers/createClassroom.ts
+++ src/handlers/createClassroom.ts
@@ -1,6 +1,11 @@
 import WebSocket from "ws"
 
-export const createClassroom = (ws: WebSocket, data: object): string => {
+import { Context } from "../"
+
+export const createClassroom = (
+    ws: WebSocket,
+    data: Context["data"]
+): string => {
     const { name } = data as { name: string }
 
     const code = [...name].reverse().join("")
--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,15 @@ const wss = new WebSocket.Server({ port: 8080 })
 
 import * as handlers from "./handlers"
 
+export interface Context {
+    type: keyof typeof handlers
+    data: object
+}
+
 wss.on("connection", ws => {
     ws.on("message", message => {
         try {
-            const parsed: {
-                type: keyof typeof handlers
-                data: object
-            } = JSON.parse(message.toString())
+            const parsed: Context = JSON.parse(message.toString())
 
             ws.send(handlers[parsed.type](ws, parsed.data))
         } catch {
