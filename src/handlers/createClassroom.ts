6ea6fbda979b5ce49dea0123b68b4b5cf281accf
BlueGhostGH <engisoftleaderoff@gmail.com>
2020-07-31 03:02:41 +0300

    feat: added a handler for creating classrooms

--- src/handlers/createClassroom.ts
+++ src/handlers/createClassroom.ts
@@ -3,6 +3,17 @@ import WebSocket from "ws"
 import { Context } from "../"
 import { State } from "../State"
 
+const freeCodes = Array(1000000).map((_, i) => i)
+
+const useCode = (name: string, state: State): number => {
+    const code = freeCodes[randomInt(0, freeCodes.length)]
+
+    freeCodes.splice(code, 1)
+    state.codes[code] = { name, host: state.lastId++, guests: [] }
+
+    return code
+}
+
 export const createClassroom = (
     ws: WebSocket,
     data: Context["data"],
@@ -10,7 +21,9 @@ export const createClassroom = (
 ): string => {
     const { name } = data as { name: string }
 
-    const code = [...name].reverse().join("")
+    const code = useCode(name, state)
 
-    return code
+    const strCode = String(code).padStart(6, "0")
+
+    return strCode
 }
