5acdeb541782d064d18183ddb5ca890f916e81f0
BlueGhostGH <engisoftleaderoff@gmail.com>
2020-07-31 07:41:22 +0300

    feat: added a handler for uploading memes

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,4 +1,5 @@
 export { createClassroom } from "./message/createClassroom"
 export { joinClassroom } from "./message/joinClassroom"
+export { uploadMeme } from "./message/uploadMeme"
 
 export { deleteClassroom } from "./close/deleteClassroom"
---
+++ src/handlers/message/uploadMeme.ts
@@ -0,0 +1,46 @@
+import WebSocket from "ws"
+
+import { Context } from "../.."
+import { State, ID } from "../../State"
+
+interface uploadMemeOptions {
+    ws: WebSocket
+    data: Context["data"]
+    state: State
+    id: ID
+}
+
+export const uploadMeme = ({
+    ws,
+    data,
+    state,
+    id
+}: uploadMemeOptions): {
+    id: number
+    url: string
+    title: string
+    username: string
+} => {
+    const { url, title } = data as { url: string; title: string }
+
+    const classroomCode = Object.entries(state.codes)
+        .map(([code, { guests }]) => {
+            const numCode = Number(code)
+
+            return [numCode, guests] as const
+        })
+        .find(([_, guests]) => guests.find(g => g.id === id))
+
+    if (!classroomCode) {
+        throw new Error("A guest outside of a classroom tried upoading a meme!")
+    }
+
+    const { "0": code } = classroomCode
+
+    const username = state.codes[code].guests.find(g => g.id === id)!.username
+    const memeId = state.lastId++
+
+    state.codes[code].memes.push({ id: memeId, url, title, username })
+
+    return { id: memeId, url, title, username }
+}
