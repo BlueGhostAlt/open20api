335e6d333bbf22eebd441e5ebc83805c86be0212
BlueGhostGH <engisoftleaderoff@gmail.com>
2020-07-31 08:05:35 +0300

    fix: made handler for deleting memes send the data to all users in a classroom

--- src/handlers/message/deleteMeme.ts
+++ src/handlers/message/deleteMeme.ts
@@ -38,5 +38,21 @@ export const deleteMeme = ({
 
     delete state.codes[code].memes[id]
 
+    const classroom = state.codes[code]
+    const users = classroom.guests
+        .concat(classroom.host)
+        .filter(u => u.id !== id)
+        .map(u => u.id)
+    const sockets = users.map(u => state.sockets.get(u)) as WebSocket[]
+
+    sockets.forEach(ws =>
+        ws.send(
+            JSON.stringify({
+                type: "deleteMeme",
+                data: { id }
+            })
+        )
+    )
+
     return { id }
 }
